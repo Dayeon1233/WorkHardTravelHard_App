dd4e715b769e807a1701f539f2586afd2d2d0652
Dayeon1233 <swdy1233@gmail.com>
2022-02-03 15:18:23 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  TouchableHighlight,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Pressable, //hitSlope 요소 바깥 어디까지 누르는 것을 감지할지 정하는 것
+  //disabled, longpress 시간을 더 정교하게 설정 가능
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <TextInput
+        placeholder={working ? "Add a To do" : "Where do you want to go?"}
+        style={styles.input}
+        keyboardType="number-pad"
+      />
     </View>
   );
 }
@@ -13,8 +52,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
